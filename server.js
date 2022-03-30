c70dc07f9352fcdaa9d0d12f673389b582646779
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-03-30 17:08:00 +0200

    server too host two react apps one on 3000 and one on 5000

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const vhost = require('vhost')
+const app = express();
+const appUpload = express();
+const port = process.env.PORT || 5000
+const port2 = process.env.PORT2 || 3000
+
+app.listen(port,()=>console.log(`Listening on port ${port}`))
+appUpload.listen(port2,()=>console.log(`Listening on port ${port2}`))
+
+app.use(express.static(path.join(__dirname, 'client/build_normal')))
+appUpload.use(express.static(path.join(__dirname, 'client/build_upload')))
+
+
+app.use('/',(req,res,next)=>{
+  return res.sendFile(path.resolve(__dirname, 'client/build_normal', 'index.html'))
+})
+appUpload.use('/',(req,res,next)=>{
+  return res.sendFile(path.resolve(__dirname, 'client/build_upload', 'index.html'))
+})
+
